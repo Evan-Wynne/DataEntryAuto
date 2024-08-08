d9d5db4505ecbf8cd9a1ee712214a63087237eb6
Evan Wynne <139228584+Evan-Wynne@users.noreply.github.com>
2024-08-08 09:58:34 +0000

    new

--- site/app.py
+++ site/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import final  # Ensure final.py contains the process_input function
+import final  # This should have the processing functions
 
 app = Flask(__name__)
 
@@ -7,13 +7,10 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         user_input = request.form['textinput']
-        # Call a processing function from final.py
-        output = final.process_input(user_input)  # This function should process the input
+        output = final.process_input(user_input)  # Make sure this function exists and does what you expect
         return render_template('index.html', user_input=user_input, output=output)
-    return render_template('index.html', output=None)
+    else:
+        return render_template('index.html', output=None)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
-
-''''''
